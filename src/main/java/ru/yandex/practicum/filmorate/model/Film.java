0a4e7b6f0676643acf2297c72f74800c581584f7
anna-sergeeva <55394206+anna-sergeeva@users.noreply.github.com>
2025-06-24 18:37:29 +0000

    Изменения внесены в соответствии с ТЗ №10

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,29 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import ru.yandex.practicum.filmorate.validation.NotBeforeDate;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.LocalDate;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Builder(toBuilder = true)
 public class Film {
+    @EqualsAndHashCode.Include
+    Long id;
+    @NotNull
+    @NotBlank
+    String name;
+    @Size(max = 200, message = "Размер описания не должен превышать 200 символов")
+    String description;
+    @NotBeforeDate()
+    LocalDate releaseDate;
+    @Positive(message = "Длительность должна быть положительной")
+    Integer duration;
 }
